30be5e4115cc3bcc28ef2555f1b5df3801e690b2
Darrius Wright <softwarebywright@gmail.com>
2019-08-18 11:30:15 -0600

    Adding tokens to push

---
+++ deploy_ci.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+echo 'https://softwarewright.dev' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f https://softwarewright:${GH_TOKEN}@github.com/softwarewright/softwarewright.github.io.git HEAD:master
+
+cd -

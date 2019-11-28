9a8708f5369be8b680ffd18c86c10e968a6be75a
softwarewright <softwarebywright@gmail.com>
2019-11-28 12:26:14 -0700

    Deploy on vuepress merge

--- deploy_ci.sh
+++ deploy_ci.sh
@@ -15,13 +15,9 @@ echo 'https://softwarewright.dev' > CNAME
 git config --global user.email "softwarebywright@gmail.com"
 git config --global user.name "Software Wright"
 
-echo "Initialized repo"
 git init
-echo "Adding files"
 git add -A
-echo "Commit deploy"
 git commit -m 'deploy'
-echo "Pushing code to deploy"
 git push -f https://softwarewright:${GH_TOKEN}@github.com/softwarewright/softwarewright.github.io.git HEAD:master
 
 cd -

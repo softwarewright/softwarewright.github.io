aa409608c703d605610dbfe3d200ce1b38cc5259
softwarewright <softwarebywright@gmail.com>
2019-11-28 12:12:03 -0700

    Supplying the GH_TOKEN to the build job

--- deploy_ci.sh
+++ deploy_ci.sh
@@ -12,10 +12,12 @@ cd docs/.vuepress/dist
 # if you are deploying to a custom domain
 echo 'https://softwarewright.dev' > CNAME
 
+echo "Initialized repo"
 git init
+echo "Adding files"
 git add -A
+echo "Commit deploy"
 git commit -m 'deploy'
-
 echo "Pushing code to deploy"
 git push -f https://softwarewright:${GH_TOKEN}@github.com/softwarewright/softwarewright.github.io.git HEAD:master
 
